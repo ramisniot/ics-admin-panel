8207be711a1c2224f3ddb18ad50de0979f0b0d87
Rami <45389071+ramisniot@users.noreply.github.com>
2020-05-13 08:07:06 +0530

    Code cleanup causing the security thread
    Code cleanup causing the security thread.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,1 @@
-User.create!([
-    {email: "ramisn@email.com", password: "tvslsl$768", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 0, current_sign_in_at: nil, last_sign_in_at: nil, current_sign_in_ip: nil, last_sign_in_ip: nil, name: "Rami"},
-    {email: "vijay.sivaraman@tvslsl.com", password: "tvslsl$768", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 0, current_sign_in_at: nil, last_sign_in_at: nil, current_sign_in_ip: nil, last_sign_in_ip: nil, name: "Vijay S"},
-    {email: "tvslslppk@email.com", password: "tvslsl@123", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 0, current_sign_in_at: nil, last_sign_in_at: nil, current_sign_in_ip: nil, last_sign_in_ip: nil, name: "TVSLSLPPK"},
-    {email: "ramakanth.reddy@tvslsl.com", password: "tvslsl$768", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 0, current_sign_in_at: nil, last_sign_in_at: nil, current_sign_in_ip: nil, last_sign_in_ip: nil, name: "Ramakanth"},
-    {email: "vimalesh@tvslsl.com", password: "tvslsl$768", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 0, current_sign_in_at: nil, last_sign_in_at: nil, current_sign_in_ip: nil, last_sign_in_ip: nil, name: "Vimalesh"},
-    {email: "farook.basha@tvslsl.com", password: "tvslsl$768", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 0, current_sign_in_at: nil, last_sign_in_at: nil, current_sign_in_ip: nil, last_sign_in_ip: nil, name: "Frook"},
-    {email: "manikandan.t@tvslsl.com", password: "tvslsl$768", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 0, current_sign_in_at: nil, last_sign_in_at: nil, current_sign_in_ip: nil, last_sign_in_ip: nil, name: "Manikandan T"}
-])
+
